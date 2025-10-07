2ed50e368845278a317e3672e73532145696c32f
Albin Preis <albin.preis@omegapoint.se>
2025-10-07 16:32:55 +0200

    tmux nav

---
+++ .config/nvim/lua/plugins/tmux-navigator.lua
@@ -0,0 +1,19 @@
+return {
+	"christoomey/vim-tmux-navigator",
+    event = "VeryLazy",
+	cmd = {
+		"TmuxNavigateLeft",
+		"TmuxNavigateDown",
+		"TmuxNavigateUp",
+		"TmuxNavigateRight",
+		"TmuxNavigatePrevious",
+		"TmuxNavigatorProcessList",
+	},
+	keys = {
+		{ "<M-h>", "<cmd><C-U>TmuxNavigateLeft<cr>" },
+		{ "<M-j>", "<cmd><C-U>TmuxNavigateDown<cr>" },
+		{ "<M-k>", "<cmd><C-U>TmuxNavigateUp<cr>" },
+		{ "<M-l>", "<cmd><C-U>TmuxNavigateRight<cr>" },
+		{ "<M-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
+	},
+}
